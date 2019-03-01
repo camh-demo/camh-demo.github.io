9d405b0413173d03cb166b61ddea9c30423defb2
Kapil <kapilsreed@gmail.com>
2019-03-01 05:51:37 -0500

    update index and init

--- js/init.js
+++ js/init.js
@@ -19,18 +19,20 @@ $(function() {
 		wineColourTop:'#AB0000',
 		wineStartY:480,
 		wineHeight:372,
-		wineLevel:0,
+		wineLevel:100,
 		maskPath:[{x:110, y:481}, {x:132, y:476}, {x:131, y:461}, {x:131, y:321}, {x:132, y:185}, {x:126, y:165}, {x:109, y:153}, {x:95, y:144}, {x:97, y:108}, {x:77, y:109}, {x:55, y:109}, {x:54, y:144}, {x:41, y:154}, {x:24, y:163}, {x:22, y:180}, {x:21, y:364}, {x:22, y:462}, {x:22, y:483}, {x:42, y:482}, ]
     });
 
 	var handle = $( "#wine-handle" );
 	$( "#wineSlider" ).slider({
 		create: function() {
-			handle.text( $( this ).slider( "value" ) + '%' );
+      var percent = 100 - $(this).slider("value");
+			handle.text( percent + '%' );
 		},
 		slide: function( event, ui ) {
-			handle.text( ui.value + '%' );
-			wineData.level = ui.value;
+      var percent = 100 - ui.value
+			handle.text( percent + '%' );
+			wineData.level =  percent;
 			$('#wineResult').val(alcoholCalc(mlWine, percentWine, wineData.level));
 
 			clearTimeout(wineData.interval);
@@ -46,18 +48,20 @@ $(function() {
 		wineStartY:666,
 		wineHeight:580,
 		wineWidth:170,
-		wineLevel:0,
+		wineLevel:100,
 		maskPath:[{x:150, y:670}, {x:178, y:656}, {x:174, y:622}, {x:175, y:437}, {x:174, y:269}, {x:160, y:196}, {x:126, y:158}, {x:123, y:129}, {x:122, y:72}, {x:90, y:75}, {x:55, y:73}, {x:54, y:124}, {x:51, y:157}, {x:14, y:203}, {x:4, y:272}, {x:3, y:449}, {x:5, y:621}, {x:1, y:653}, {x:28, y:669}, ]
     });
 
 	var vodkahandle = $( "#vodka-handle" );
 	$( "#vodkaSlider" ).slider({
 		create: function() {
-			vodkahandle.text( $( this ).slider( "value" ) + '%' );
+      var percent = 100 - $(this).slider("value");
+			vodkahandle.text( percent + '%' );
 		},
 		slide: function( event, ui ) {
-			vodkahandle.text( ui.value + '%' );
-			vodkaData.level = ui.value;
+      var percent = 100 - ui.value
+			vodkahandle.text( percent + '%' );
+			vodkaData.level = percent;
 			$('#vodkaResult').val(alcoholCalc(mlVodka, percentVodka, vodkaData.level));
 
 			clearTimeout(vodkaData.interval);
@@ -73,18 +77,20 @@ $(function() {
 		wineStartY:400,
 		wineHeight:360,
 		wineWidth:110,
-		wineLevel:0,
+		wineLevel:100,
 		maskPath:[{x:109, y:405}, {x:133, y:402}, {x:130, y:375}, {x:131, y:282}, {x:131, y:206}, {x:134, y:187}, {x:119, y:150}, {x:108, y:99}, {x:96, y:45}, {x:77, y:46}, {x:53, y:43}, {x:45, y:99}, {x:31, y:148}, {x:22, y:174}, {x:18, y:201}, {x:18, y:270}, {x:20, y:376}, {x:20, y:403}, {x:43, y:406}, ]
     });
 
 	var beerhandle = $( "#beer-handle" );
 	$( "#beerSlider" ).slider({
 		create: function() {
-			beerhandle.text( $( this ).slider( "value" ) + '%' );
+      var percent = 100 - $(this).slider("value");
+			beerhandle.text( percent + '%' );
 		},
 		slide: function( event, ui ) {
-			beerhandle.text( ui.value + '%' );
-			beerData.level = ui.value;
+      var percent = 100 - ui.value
+			beerhandle.text( percent + '%' );
+			beerData.level = percent;
 			$('#beerResult').val(alcoholCalc(mlBeer, percentBeer, beerData.level));
 
 			clearTimeout(beerData.interval);
